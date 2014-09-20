7e59a6b1bf5d80a79e57dbc02db658e0afc6d916
Justin Grathwohl <justin.grathwohl@gmail.com>
2014-09-20 16:10:23 -0700

    Update Change Local Admins (ADSI).ps1
    This script is aimed to disable the Windows built-in local administrator account and create a new local user account with administrator privileges. It will also set the password of each account via input in the console window. This script traverses the AD OU structure, picks up computer object names, sorts them, and performs the actions using ADSI controls. It will also log a CSV file of all offline or otherwise unreachable computers (broken admin share or RPC services, typically).
    
    Side note: in my organization, we have an OU where we place digital signage, POS terminals, and customer-facing kiosks that need a specific set of GPOs applied to them and thus have GP inheritance disabled on their top OU aptly named "No GP Inheritance". The script modifies the ADSI/LDAP path based on the initial input of the script. It will ask for a computer name (if any) to target a specific computer in an OU, the OU computer objects rest in (if any), and if it's in the no GP inheritance OU. If a computer is defined, an OU MUST be defined unless it rests in the base computer OU.
    
    Ex:
    
    This would hit a computer in the base computer OU only:
    CN=Test1,OU=Computers,OU=Test,DC=domain,DC=com
    
    This would hit all computers in a specificed OU (no computer defined at prompt):
    OU=Test1,OU=Computers,OU=Test,DC=domain,DC=com
    
    This would hit all computers in the No GP Inheritance OU:
    OU=No GP Inheritance,OU=Computers,OU=Test,DC=domain,DC=com
    
    This would hit all computers in an OU under No GP Inheritance:
    OU=Test1,OU=No GP Inheritance,OU=Computers,OU=Test,DC=domain,DC=com
    
    Based on user input, this would be dynamic.

--- Change Local Admins (ADSI).ps1
+++ Change Local Admins (ADSI).ps1
@@ -2,7 +2,28 @@ Import-Module activedirectory
 
 $ErrorActionPreference = "Continue"
 
-$searchbase = "OU=Testing,DC=domain,DC=com"
+Write-Host "Checking if log folder exists and making it if not"
+$Path = "C:\PowerShell Logs"
+If (!(Test-Path -Path $Path ))
+{
+	New-Item -ItemType Directory -Path $Path
+}	
+Else
+{
+	Write-Host "Folder exists, proceeding with script execution"
+}	
+$baseAD = "OU=Computers,OU=Test,DC=domain,DC=com"
+$Date = Get-Date -format MM.dd.yyyy
+
+$Comp = Read-Host "Please define a computer"
+$OU = Read-Host "Please define an OU"
+$askNoGPI = Read-Host "Are the computers under the No GP Inheritance OU? (y/n)"
+
+$searchbase = $baseAD
+
+if ($askNoGPI -eq 'y') { $searchbase = "OU=No GP Inheritance,$searchbase" }
+if ($OU) { $searchbase = "OU=$OU,$searchbase" }
+if ($Comp) { $searchbase = "CN=$Comp,$searchbase" }
 
 $query = Get-ADComputer -Filter * -SearchBase $searchbase | Select Name | Sort Name
 $computers = $query.Name
@@ -14,32 +35,31 @@ $LocalUserPW = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.Inter
 
 foreach ($computer in $computers)
 {
-    Write-Host "Connecting..."
+	Write-Host "Connecting..."
 	If(Test-Connection $computer -Count 1 -Quiet) #Check that PC is online
     {
-		Write-Host "Using ADSI..."
-		Write-Host "Changing local admin password on $computer!"
+		Write-Host "Changing local admin password on $computer"
 		$LocalAdmin = [adsi]"WinNT://$computer/Administrator,user"
 		$LocalAdmin.SetPassword($LocalAdminPW)
 		$LocalAdmin.SetInfo()
 		$LocalAdmin.InvokeSet("UserFlags",($LocalAdmin.UserFlags[0] -BOR 2))
 		$LocalAdmin.SetInfo()
-		Write-Host "Local admin password is set and account disabled!"
+		Write-Host "Local admin password is set and account disabled"
 		$LocalComputer = ([adsi]"WinNT://$computer,computer")
 		$LocalComputerUsers = ($LocalComputer.psbase.children | Where-Object {$_.psBase.schemaClassName -eq "User"} | Select-Object -expand Name)
 		$LCUFound = $LocalComputerUsers -contains "AG"
 		$LocalUser = [adsi]"WinNT://$computer/AG,user"
 		If($LCUFound)
 		{
-		Write-Host "Local user already exists, setting password and user properties!"
+		Write-Host "Local user already exists, setting password and user properties"
 		$LocalUser.SetPassword($LocalUserPW)
 		$LocalUser.InvokeSet("UserFlags",($LocalUser.UserFlags[0] -BXOR 2))
 		$LocalUser.SetInfo()
-		Write-Host "Local user modified and password set!"		
+		Write-Host "Local user modified and password set"		
 		}
 		Else
 		{
-		Write-Host "New local admin does not exist, creating!"
+		Write-Host "New local admin does not exist, creating"
 		$NewUser = "AG"
 		$CreateLocalUser = $LocalComputer.Create('User', $NewUser)
 		$CreateLocalUser.SetPassword($LocalUserPW)
@@ -47,11 +67,17 @@ foreach ($computer in $computers)
 		$LocalUser.InvokeSet("UserFlags",($LocalUser.UserFlags[0] -BXOR 2))
 		$LocalUser.SetInfo()
 		([adsi]"WinNT://$computer/Administrators,group").Add("WinNT://$computer/AG")
-		Write-Host "New user created, password set, and added to local admin group!"
+		Write-Host "New user created, password set, and added to local admin group"
 		}
 	}
 	else
 	{
-		Write-Host -ForegroundColor Red "Can't connect to $computer!"
+		$offline = $computer | Out-File -Append -noClobber -filePath "C:\PowerShell Logs\Offline Computers $Date.csv" -width 20
+		Write-Host -ForegroundColor Red "Can't connect to $computer"
+		$offline
+		Write-Host "Added to offline log file for $Date"
 	}
 }
+Write-Host "All offline computers have been written to the log file"
+Write-Host "Opening log file..."
+Invoke-Item "C:\PowerShell Logs\Offline Computers $Date.csv"
