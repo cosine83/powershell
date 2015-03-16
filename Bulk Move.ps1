81841d9a370bbbbf5d867fbbe75a319ccd4eb431
Justin Grathwohl <justin.grathwohl@gmail.com>
2015-03-16 15:40:35 -0700

    Update Bulk Move.ps1

--- Bulk Move.ps1
+++ Bulk Move.ps1
@@ -4,12 +4,42 @@ Import-Module NTFSSecurity
 Import-Module Activedirectory
 
 $ErrorActionPreference = "Continue"
+
 $Property = Read-Host "What property's file server are you migrating?"
-if ($Property -eq "WV") {
-	$OldServer = "wvfileserv1s"
+if ($Property -eq "X1") {
+	$HomeOwn = "\\oldfileserv1s\n$\Home Drives"
+	$DeptOwn = "\\oldfileserv1s\n$\DeptShares\"
+}
+elseif ($Property -eq "X2") { 
+	$HomeOwn = "\\oldfileserv1v\d$\Home\"
+	$DeptOwn = "\\oldfileserv1v\d$\DeptShares\"
+}
+elseif ($Property -eq "X3") { 
+	$HomeOwn = "\\oldfileserv1s\d$\Home Shares"
+	$DeptOwn = "\\oldfileserv1s\d$\DeptShares\"
+}
+else { 
+	Write-Host "Property designation invalid. Try again."
+}
+
+$TakeOwn = Read-Host "Do you need to take ownership of files and folders first?"
+if ($TakeOwn -eq "y") {
+Write-Host "Taking ownership of home folders"
+takeown /a /r /d y /f $HomeOwn | Out-Null
+Write-Host "Ownership given to local admins group"
+Write-Host "Taking ownership of department folders"
+takeown /a /r /d y /f $DeptOwn | Out-Null
+Write-Host "Ownership given to local admins group"
+}
+else {
+	Write-Host "Skipping ownership, proceeding to setting security permissions"
+}
+
+if ($Property -eq "X") {
+	$OldServer = "oldfileserv1s"
 	$OldDept = "\\$($OldServer)\d$\Shares"
 	$OldHome = "\\$($OldServer)\h$\Shares"
-	$NewServer = "wvfileserv1v"
+	$NewServer = "newfileserv1v"
 	$NewDept = "\\$($NewServer)\d$\DeptShares"
 	$ArchiveDept = "\\$($NewServer)\d$\_ArchiveOldFiles\Dept" 
 	$DeptTapeArchive = "\\$($NewServer)\d$\_ArchiveOldFiles\Tape\Dept"
@@ -19,15 +49,23 @@ if ($Property -eq "WV") {
 	$HomeTapeArchive = "\\$($NewServer)\d$\_ArchiveOldFiles\Tape\Home"
 	$OldHomeArchive = "\\$($NewServer)\d$\Home\_RemovedAccounts"
 }
-elseif ($Property -eq "RC") {
-	$OldServer = "rcfileserv1s"
+elseif ($Property -eq "X2") {
+	$OldServer = "oldfileserv1s"
 	$OldDept = "\\$($OldServer)\n$\DeptShares"
 	$OldHome = "\\$($OldServer)\n$\Home Drives"
-	$NewServer = "rcfileserv1v"
+	$OldMultiShares = "\\$($OldServer)\n$\MultiShares"
+	$OldSystems = "\\$($OldServer)\n$\Systems"
+	$OldPublic = "\\$($OldServer)\n$\Public"
+	$OldHomeOwn = "\\$($OldServer)\n$\Home Drives\"
+	$OldDeptOwn = "\\$($OldServer)\n$\DeptShares\"
+	$NewServer = "newfileserv1v"
 	$NewDept = "\\$($NewServer)\d$\DeptShares"
+	$NewMultiShares = "\\$($NewServer)\s$\MultiShares"
+	$NewSystems = "\\$($NewServer)\s$\Systems"
+	$NewPublic = "\\$($NewServer)\d$\DeptShares\Public"
 	$ArchiveDept = "\\$($NewServer)\d$\_ArchiveOldFiles\Dept" 
 	$DeptTapeArchive = "\\$($NewServer)\d$\_ArchiveOldFiles\Tape\Dept"
-	$OldDeptArchive = "\\$($NewServer)\d$\DeptShares\*\_*Old*"
+	$OldDeptArchive = "\\$($NewServer)\d$\DeptShares\*\*Old*"
 	$NewHome = "\\$($NewServer)\h$\HomeDrives"
 	$ArchiveHome = "\\$($NewServer)\h$\_ArchiveOldFiles\Home"
 	$HomeTapeArchive = "\\$($NewServer)\h$\_ArchiveOldFiles\Tape\Home"
@@ -40,27 +78,34 @@ else {
 
 $AgeLimit = (Get-Date).AddDays(-1461)
 $ArchiveAgeLimit = (Get-Date).AddDays(-2922)
+$CompleteTime = Get-Date
 
-robocopy $OldDept $NewDept /sec /mir /XO /XX /FFT /NFL /NDL /nc /ns /r:1 /w:10 /maxage:20110101
-robocopy $OldHome $NewHome /sec /mir /XO /XX /FFT /NFL /NDL /nc /ns /r:1 /w:10 /maxage:20110101
-robocopy $NewDept $ArchiveDept /sec /mir /XO /XX /FFT /NFL /NDL /nc /ns /r:1 /w:10 /minage:20101231 /maxage:20070101
-robocopy $NewHome $ArchiveHome /sec /mir /XO /XX /FFT /NFL /NDL /nc /ns /r:1 /w:10 /minage:20101231 /maxage:20070101
-robocopy $OldHome $ArchiveHome /sec /mir /XX /FFT /NFL /NDL /nc /ns /r:1 /w:10 /minage:20101231 /maxage:20070101
-robocopy $OldDept $ArchiveDept /sec /mir /XX /FFT /NFL /NDL /nc /ns /r:1 /w:10 /minage:20101231 /maxage:20070101
-robocopy $ArchiveDept $DeptTapeArchive /sec /mir /XX /FFT /NFL /NDL /nc /ns /r:1 /w:10 /minage:20061231
-robocopy $ArchiveHome $HomeTapeArchive /sec /mir /XX /FFT /NFL /NDL /nc /ns /r:1 /w:10 /minage:20061231
-Write-Host "Moving old archive folder to new archive location"
-Get-ChildItem -Path $OldDeptArchive -Directory -Recurse | ForEach-Object { Move-Item $ArchiveDept -Force }
-Get-ChildItem2 -Path $OldHomeArchive -Directory -Recurse | ForEach-Object { Move-Item $ArchiveHome -Force }
+Write-Host "Starting copy process"
+robocopy $OldDept $NewDept /sec /secfix /timfix /mir /XO /XX /FFT /NFL /NDL /nc /ns /r:1 /w:3 /COPYALL /ZB
+robocopy $OldHome $NewHome /sec /secfix /timfix /mir /XO /XX /FFT /NFL /NDL /nc /ns /r:1 /w:3 /COPYALL /ZB
+robocopy $OldMultiShares $NewMultiShares /sec /secfix /timfix /mir /XO /XX /FFT /NFL /NDL /nc /ns /r:1 /w:3 /COPYALL /ZB
+robocopy $OldSystems $NewSystems /sec /secfix /timfix /mir /XO /XX /FFT /NFL /NDL /nc /ns /r:1 /w:3 /ZB /COPYALL
+Write-Host "Copying complete"
+
+Write-Host "Arranging archives"
+robocopy $NewDept $ArchiveDept /sec /secfix /timfix /mir /MOV /FFT /NFL /NDL /nc /ns /r:1 /w:3 /minage:20101231 /ZB
+robocopy $NewHome $ArchiveHome /sec /secfix /timfix /mir /MOV /FFT /NFL /NDL /nc /ns /r:1 /w:3 /minage:20101231 /ZB
+robocopy $OldHome $ArchiveHome /sec /secfix /timfix /mir /MOV /XX /XO /FFT /NFL /NDL /nc /ns /r:1 /w:3 /minage:20101231 /ZB
+robocopy $OldDept $ArchiveDept /sec /secfix /timfix /mir /MOV /XX /XO /FFT /NFL /NDL /nc /ns /r:1 /w:3 /minage:20101231 /ZB
+robocopy $ArchiveDept $DeptTapeArchive /sec /secfix /timfix /mir /MOV /FFT /NFL /NDL /nc /ns /r:1 /w:3 /minage:20061231 /ZB
+robocopy $ArchiveHome $HomeTapeArchive /sec /secfix /timfix /mir /MOV /FFT /NFL /NDL /nc /ns /r:1 /w:3 /minage:20061231 /ZB
+Write-Host "Archive arrangement complete"
+
+Write-Host "Moving old archive files/folders to new archive location"
+Get-ChildItem2 -Path $OldDeptArchive -Recurse | ForEach-Object { Move-Item $ArchiveDept -Force }
+Get-ChildItem2 -Path $OldHomeArchive -Recurse | ForEach-Object { Move-Item $ArchiveHome -Force }
 Write-Host "Move complete"
 
 Write-Host "Removing duplicate and empty files and folders"
-Get-ChildItem2 -Path $NewDept -Recurse | Where { !$_.PSIsContainer -and $_.LastWriteTime -lt $AgeLimit } | Remove-Item -Force
-Get-ChildItem2 -Path $NewDept -Recurse | Where { $_.PSIsContainer -and (Get-ChildItem2 -Path $_.FullName -Recurse | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
-Get-ChildItem2 -Path $NewHome -Recurse | Where { !$_.PSIsContainer -and $_.LastWriteTime -lt $AgeLimit } | Remove-Item -Force
-Get-ChildItem2 -Path $NewHome -Recurse | Where { $_.PSIsContainer -and (Get-ChildItem2 -Path $_.FullName -Recurse | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
-Get-ChildItem2 -Path $ArchiveHome -Recurse | Where { !$_.PSIsContainer -and $_.LastWriteTime -lt $ArchiveAgeLimit } | Remove-Item -Force
-Get-ChildItem2 -Path $ArchiveHome -Recurse | Where { $_.PSIsContainer -and (Get-ChildItem2 -Path $_.FullName -Recurse | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
-Get-ChildItem2 -Path $ArchiveDept -Recurse | Where { !$_.PSIsContainer -and $_.LastWriteTime -lt $ArchiveAgeLimit } | Remove-Item -Force
-Get-ChildItem2 -Path $ArchiveDept -Recurse | Where { $_.PSIsContainer -and (Get-ChildItem2 -Path $_.FullName -Recurse | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
-Write-Host "Script Complete"
+Get-ChildItem2 -Path $NewDept -Recurse | Where { $_.LastWriteTime -lt $AgeLimit } | Remove-Item -Recurse -Force
+Get-ChildItem2 -Path $NewHome -Recurse | Where { $_.LastWriteTime -lt $AgeLimit } | Remove-Item -Recurse -Force
+Get-ChildItem2 -Path $ArchiveHome -Recurse | Where { $_.LastWriteTime -lt $ArchiveAgeLimit } | Remove-Item -Recurse -Force
+Get-ChildItem2 -Path $ArchiveDept -Recurse | Where { $_.LastWriteTime -lt $ArchiveAgeLimit } | Remove-Item -Recurse -Force
+Write-Host "Removal complete"
+
+Write-Host "Script Completed at $CompleteTime"
