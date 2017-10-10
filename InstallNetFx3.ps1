513694bd0a20e389e604a9d716e3d220fd0030d0
Justin Grathwohl <justin.grathwohl@gmail.com>
2017-10-10 15:45:53 -0700

    Update InstallNetFx3.ps1

--- InstallNetFx3.ps1
+++ InstallNetFx3.ps1
@@ -1,6 +1,6 @@
 #Requires microsoft-windows-netfx3-ondemand-package.cab from the install media under \Sources\SxS
 #Syntax for SCCM Task Sequence only, unsure of usage outside of that
-#Put in as a step after Windows setup and configuration as "Run PowerShell Script"
+#Put in as a "Run PowerShell Script" step after Windows setup and configuration
 
 $currentLocation = Split-Path -Parent $MyInvocation.MyCommand.Path;
 Enable-WindowsOptionalFeature -Online -FeatureName NetFx3 -Source $currentLocation -LimitAccess -All
