1fc562fd05a97ba0072f3bcd90370a554ea0af63
Justin Grathwohl <justin.grathwohl@gmail.com>
2019-09-13 12:19:46 -0700

    Update Fix-ExOContacts.ps1

--- Fix-ExOContacts.ps1
+++ Fix-ExOContacts.ps1
@@ -15,15 +15,19 @@ $userName = $user.Name
 		Write-Host "$userName already has targetAddress set, check for Exchange Online mailbox"
 	}
 }
-
+Write-Host "Starting AADC delta sync"
 Start-ADSyncSyncCycle -PolicyType Delta
+
+Write-Host "Waiting for 5 minutes..."
 Start-Sleep -Seconds 300
 
 $targetAddressException = Get-AdUser -Filter {Enabled -eq $true} -Properties targetAddress | Where {$_.targetAddress -notlike "*.onmicrosoft.com"}
 $userException = $targetAddressException.SamAccountName
 
+Write-Host "Clearing targetAddress field on non-Exchange Online users"
 ForEach ($user in $userException) {
 	Set-ADUser $user -Clear targetAddress
 }
 
+Write-Host "Starting final delta sync"
 Start-ADSyncSyncCycle -PolicyType Delta
