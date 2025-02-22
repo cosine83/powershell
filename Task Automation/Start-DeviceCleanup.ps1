69a21f9e35e5d884326eba224bdc31480a9ef678
Justin Grathwohl <justin.grathwohl@gmail.com>
2025-02-21 19:35:41 -0800

    fix array

--- Task Automation/Start-DeviceCleanup.ps1
+++ Task Automation/Start-DeviceCleanup.ps1
@@ -54,7 +54,7 @@ $cleanupFolders = @(
     "C:\Temp",`
     "C:\Windows\System32\LogFiles",`
     "C:\inetpub\logs",`
-    "C:\Windows\System32\winevt\Logs",`
+    "C:\Windows\System32\winevt\Logs"
 )
 
 # Create array for Cleanmgr registry keys
